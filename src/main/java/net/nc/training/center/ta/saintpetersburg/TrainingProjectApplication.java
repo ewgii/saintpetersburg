a6cc380ea19fdb45811f388885700b7b3427d328
eves1118 <evgenii.eskov@netcracker.com>
2021-03-21 14:22:52 +0400

    delete security block

--- src/main/java/net/nc/training/center/ta/saintpetersburg/TrainingProjectApplication.java
+++ src/main/java/net/nc/training/center/ta/saintpetersburg/TrainingProjectApplication.java
@@ -2,6 +2,9 @@ package net.nc.training.center.ta.saintpetersburg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TrainingProjectApplication {
@@ -9,5 +12,18 @@ public class TrainingProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingProjectApplication.class, args);
 	}
+
+
+
+	@Bean
+	public WebMvcConfigurer corsConfig() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
+
 
 }
