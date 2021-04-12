f7123aea789242cefc71a61d4de399b5727b7d39
amashq <A.Mashenkova@gmail.com>
2021-04-12 19:16:26 +0300

    add host https://saintpetersburgfront-911bf4du3-ewgii.vercel.app/

--- src/main/java/net/nc/training/center/ta/saintpetersburg/TrainingProjectApplication.java
+++ src/main/java/net/nc/training/center/ta/saintpetersburg/TrainingProjectApplication.java
@@ -9,20 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class TrainingProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrainingProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingProjectApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfig() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:8080", "https://saintpetersburgfront.vercel.app");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://saintpetersburgfront.vercel.app",
+                                "https://saintpetersburgfront-911bf4du3-ewgii.vercel.app/");
+            }
+        };
+    }
 
 
 }
